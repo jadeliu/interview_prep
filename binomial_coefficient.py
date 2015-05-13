d7b4a17e8d4671aedfe4e8442b4f86f55bcad84f
jadeliu1222 <jadeliu1222@gmail.com>
2015-05-12 20:55:44 -0700

    bug fixed

--- binomial_coefficient.py
+++ binomial_coefficient.py
@@ -2,14 +2,14 @@ __author__ = 'qiong'
 # for epi 12.14
 
 def compute_binomial_coefficent(n, k):
-    table = [[0 for j in k+1] for i in n+1]
+    table = [[0 for j in range(k+1)] for i in range(n+1)]
 
 
     for i in range(n+1):
         table[i][0] = 1
 
     for j in range(1, k+1):
-        table[1][j] = 1
+        table[j][j] = 1
 
     for i in range(2, n+1):
         for j in range(1, k+1):
@@ -19,3 +19,8 @@ def compute_binomial_coefficent(n, k):
                 break
 
     return table[n][k]
+
+
+print compute_binomial_coefficent(5, 1)
+print compute_binomial_coefficent(1, 1)
+print compute_binomial_coefficent(6, 2)
